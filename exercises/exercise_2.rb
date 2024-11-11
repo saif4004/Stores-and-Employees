5acfa818c2ed6cc20b67ae29a5765a5722fef772
Saif Al-saedi <saif4004@gmail.com>
2024-11-11 15:42:53 -0800

    Assigned @store1 and @store2 to stores with IDs 1 and 2 and updated @store1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,10 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
-
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1.name
+@store1.name = "Surrey"
+puts @store1.name
